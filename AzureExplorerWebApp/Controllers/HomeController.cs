c4acbe5cbe808b9be686d69a565acb6795dc97bd
Amit Kekan <v-amkek@microsoft.com>
2021-07-12 11:44:47 -0700

    Disabled authentication

--- AzureExplorerWebApp/Controllers/HomeController.cs
+++ AzureExplorerWebApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
