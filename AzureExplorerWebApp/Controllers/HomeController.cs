6774a52513a37708e31a2ea63f8436f807b86df3
Amit Kekan <v-amkek@microsoft.com>
2021-08-01 22:36:24 -0700

    Updated base messages, added about page

--- AzureExplorerWebApp/Controllers/HomeController.cs
+++ AzureExplorerWebApp/Controllers/HomeController.cs
@@ -30,6 +30,11 @@
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
