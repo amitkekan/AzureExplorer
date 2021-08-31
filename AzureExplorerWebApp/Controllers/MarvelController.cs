d25725deba6255fdf7b355d8c511e85dbd7f841c
Amit Kekan <amit.kekan@live.com>
2021-08-31 14:07:51 -0700

    Adding Api Controller for test

---
+++ AzureExplorerWebApp/Controllers/MarvelController.cs
@@ -0,0 +1,21 @@
+﻿namespace AzureExplorerWebApp.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarvelController : ControllerBase
+    {
+        [Route("test")]
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Test Api Controller");
+        }
+    }
+}
