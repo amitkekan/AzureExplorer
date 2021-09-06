5cf08e87bc988b677879401fb0a870c4f88604c2
Amit Kekan <amit.kekan@live.com>
2021-09-05 23:48:06 -0700

    Marvel api comics, characters endpoint

--- AzureExplorerWebApp/Controllers/MarvelController.cs
+++ AzureExplorerWebApp/Controllers/MarvelController.cs
@@ -1,21 +1,59 @@
 ﻿namespace AzureExplorerWebApp.Controllers
 {
+    using AzureExplorerWebApp.Helpers;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using System.Security.Cryptography;
+    using System.Text;
 
     [Route("api/[controller]")]
     [ApiController]
     public class MarvelController : ControllerBase
     {
+        private readonly ILogger<MarvelController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        public MarvelController(ILogger<MarvelController> logger, IConfiguration Configuration)
+        {
+            _logger = logger;
+            _configuration = Configuration;
+        }
+
         [Route("test")]
         [HttpGet]
-        public IActionResult Test()
+        public async Task<IActionResult> Test()
         {
-            return Ok("Test Api Controller");
+            var publicKey = KeyVaultHelper.GetManagedKeyVaultSecret(_configuration, "Marvel-Public-Key");
+            var privateKey = KeyVaultHelper.GetManagedKeyVaultSecret(_configuration, "Marvel-Private-Key");
+
+            var timeStamp = DateTime.Now.Ticks.ToString();
+
+            var hash = this.GetHash(timeStamp, publicKey, privateKey);
+
+            var result = await HttpHelper.GetResult(string.Format("http://gateway.marvel.com/v1/public/comics?ts={0}&apikey={1}&hash={2}", timeStamp, publicKey, hash));
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ts"></param>
+        /// <param name="publicKey"></param>
+        /// <param name="privateKey"></param>
+        /// <returns></returns>
+        private string GetHash(string ts, string publicKey, string privateKey)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(ts + privateKey + publicKey);
+            var generator = MD5.Create();
+            byte[] bytesHash = generator.ComputeHash(bytes);
+            return BitConverter.ToString(bytesHash).ToLower().Replace("-", String.Empty);
         }
     }
 }
---
+++ AzureExplorerWebApp/Helpers/HttpHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace AzureExplorerWebApp.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public static class HttpHelper
+    {
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        public static async Task<string> GetResult(string httpUrl)
+        {
+            try
+            {                
+                string responseBody = await client.GetStringAsync(httpUrl);
+
+                // stream responseJson = await client.GetStreamAsync(httpUrl);
+                // var responseObject = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                return ex.Message;
+            }
+
+            return string.Empty;
+        }
+    }
+}
